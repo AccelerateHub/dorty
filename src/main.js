afdabeb5ff4f71fcec1b257469ebbce392f4dcea
Wisdom Matthew <talk2wisdommatt@gmail.com>
2020-12-26 20:35:30 +0100

    Added Google Maps to add dorti page.

--- src/main.js
+++ src/main.js
@@ -4,11 +4,21 @@ import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import * as VueGoogleMaps from "vue2-google-maps"
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
-    // Optionally install the BootstrapVue icon components plugin
+
+// Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
+
+// Using Google Maps
+Vue.use(VueGoogleMaps, {
+    load: {
+        key: ''
+    },
+    installComponents: true
+})
 
 Vue.config.productionTip = false
 
